3168d0f6b1afaa92eee47279c77d20735c84726a
Borucinski, Michal <mborucinski@kpmg.pl>
2020-04-25 20:38:54 +0200

    UI fixing and final working application.

--- WeddingWeb/ClientApp/src/app/navbar/navbar.component.ts
+++ WeddingWeb/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,15 +1,70 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, AfterViewInit, HostBinding } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { fromEvent } from 'rxjs';
+import { throttleTime, map, pairwise, distinctUntilChanged, share, filter } from 'rxjs/operators';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ opacity: 0, transform: 'translateY(-100%)' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('300ms ease-in'))
+    ])
+  ]
 })
-export class NavbarComponent { }
+export class NavbarComponent implements AfterViewInit {
+  private isVisible = true;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
+  }
+
+  ngAfterViewInit(): void {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+
+    const scrollUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const scrollDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    scrollUp$.subscribe(() => this.isVisible = true);
+    scrollDown$.subscribe(() => (this.isVisible = false));
+  }
+}
 
 @NgModule({
   imports: [
