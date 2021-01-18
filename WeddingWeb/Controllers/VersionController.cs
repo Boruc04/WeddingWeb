6ce60b0aa60585561192ab8f73d9f5204b0f8c2a
Borucinski, Michal <mborucinski@kpmg.pl>
2021-01-18 23:32:56 +0100

    Introduce version service

---
+++ WeddingWeb/Controllers/VersionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeddingWeb.Services;
+using Version = WeddingWeb.Services.Version;
+
+namespace WeddingWeb.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	//[ApiVersion("1.0")]
+	[Produces("application/json")]
+	public class VersionController : ControllerBase
+	{
+		private readonly VersionService _versionService;
+
+		public VersionController()
+		{
+			_versionService = new VersionService();
+		}
+
+		[HttpGet]
+		[Route("")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public async Task<IActionResult> Get()
+		{
+			var version = await _versionService.GetApplicationVersion();
+			return Ok(version);
+		}
+	}
+}
---
+++ WeddingWeb/Services/VersionService.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WeddingWeb.Services
+{
+	public class VersionService
+	{
+		private readonly Version _version;
+
+		public VersionService()
+		{
+			_version = new Version
+			{
+				Value = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+			};
+		}
+
+		public Task<Version> GetApplicationVersion() => Task.FromResult(_version);
+	}
+
+	public class Version
+	{
+		public string Value { get; set; }
+	}
+}
