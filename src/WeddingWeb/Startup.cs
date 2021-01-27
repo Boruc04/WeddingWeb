acd988be154c7f79673a7b4f366c3e89ca1feac0
Borucinski, Michal <mborucinski@kpmg.pl>
2021-01-27 21:30:28 +0100

    Removing razer pages and related functionality.

--- src/WeddingWeb/Startup.cs
+++ src/WeddingWeb/Startup.cs
@@ -43,7 +43,6 @@ namespace WeddingWeb
 			}
 			else
 			{
-				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
