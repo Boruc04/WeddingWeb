3a3fa1e3fcb79264b95cf1feb1e0e7b031d45ceb
Borucinski, Michal <mborucinski@kpmg.pl>
2021-02-11 20:30:20 +0100

    Fixing sonar cloud issues.

--- src/WeddingWeb/ClientApp/src/main.ts
+++ src/WeddingWeb/ClientApp/src/main.ts
@@ -4,11 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { AppInjector } from './app/app-injector.service';
 
 if (environment.production) {
   enableProdMode();
 }
-
 
 platformBrowserDynamic().bootstrapModule(AppModule);
