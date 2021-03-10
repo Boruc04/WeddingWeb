82171d59e95da381963f3c92e2a812b0efd5f6de
Michał Boruciński <m.borucinski@gmail.com>
2021-03-10 20:46:08 +0100

    Add app service to terraform (#75)
    * Add app service to terraform
    
    * Fix provide and backend name + change app service name
    
    * Replaceing exammple with test rg
    
    Co-authored-by: Borucinski, Michal <mborucinski@kpmg.pl>

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
       version = ">= 2.26"
     }
   }
-  
+
   backend "azurerm" {
   }
 }
@@ -15,7 +15,29 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "test" {
   name     = "wedding-eu-test-rg"
   location = "westeurope"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "wedding-web-test-asp"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = "wedding-web-test-app"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  app_service_plan_id = azurerm_app_service_plan.test.id
+
+  app_settings = {
+    "ASPNETCORE_ENVIRONMENT" = "Test"
+  }
 }
