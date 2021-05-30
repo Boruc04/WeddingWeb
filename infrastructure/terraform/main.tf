282c3d8a24aa752d082d9a1fdd6eb7f34d14ed4f
Borucinski, Michal <mborucinski@kpmg.pl>
2021-05-30 18:00:05 +0200

    Extending terraform with keyvault and mi

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,12 +2,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 2.26"
     }
   }
 
   backend "azurerm" {
+    resource_group_name  = "boruc-common-eu-rg"
+    storage_account_name = "weddingwebterraform"
+    container_name       = "terraform"
+    key                  = "test.terraform.tfstate"
   }
 }
 
@@ -15,29 +19,85 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "test" {
-  name     = "wedding-eu-test-rg"
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "wedding-eu-${var.prefix}-rg"
   location = "westeurope"
 }
 
-resource "azurerm_app_service_plan" "test" {
-  name                = "wedding-web-test-asp"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+resource "azurerm_user_assigned_identity" "mi" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  name                = "wedding-web-eu-${var.prefix}-mi"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "wedding-eu-${var.prefix}-kv"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get", "List", "Create", "Delete"
+    ]
+
+    secret_permissions = [
+      "Get", "List", "Set", "Delete"
+    ]
+
+    storage_permissions = [
+      "Get", "List", "Set", "Delete"
+    ]
+  }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.mi.principal_id
+
+    key_permissions = [
+      "Get"
+    ]
+
+    secret_permissions = [
+      "Get"
+    ]
+
+    storage_permissions = [
+      "Get"
+    ]
+  }
+
+}
+
+resource "azurerm_app_service_plan" "webapp" {
+  name                = "wedding-web-${var.prefix}-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = var.appserviceplan_sku.tier
+    size = var.appserviceplan_sku.size
   }
 }
 
-resource "azurerm_app_service" "test" {
-  name                = "wedding-web-test-app"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  app_service_plan_id = azurerm_app_service_plan.test.id
+resource "azurerm_app_service" "wedding-web" {
+  name                = "wedding-web-${var.prefix}-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.webapp.id
+  https_only          = true
 
   app_settings = {
-    "ASPNETCORE_ENVIRONMENT" = "Test"
+    "ASPNETCORE_ENVIRONMENT" = "${var.ASPNETCORE_ENVIRONMENT}"
   }
 }
+
