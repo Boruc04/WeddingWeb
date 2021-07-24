f5012a08ffceca8dee64e66c63e0c9ecb36cfad7
Borucinski, Michal <mborucinski@kpmg.pl>
2021-07-24 09:09:11 +0200

    Intro web gallery

--- src/WeddingWeb/ClientApp/src/app/app-routing.module.ts
+++ src/WeddingWeb/ClientApp/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { ConfirmComponent } from './confirm/confirm.component';
 import { ContactComponent } from './contact/contact.component';
 import { AddressComponent } from './address/address.component';
-
+import { PhotoComponent } from './gallery/photo/photo.component';
+import { VideoComponent } from './gallery/video/video.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'confirm', component: ConfirmComponent },
   { path: 'address', component: AddressComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'gallery', component: PhotoComponent },
+  { path: 'video', component: VideoComponent }
 ];
 
 @NgModule({
