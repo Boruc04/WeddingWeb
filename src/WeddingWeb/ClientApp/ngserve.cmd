29ae188cbd54383e3917b82f80768c546e7978c5
Borucinski, Michal <mborucinski@kpmg.pl>
2021-07-27 20:09:38 +0200

    Hack for running angular 11 application from dotnet project

---
+++ src/WeddingWeb/ClientApp/ngserve.cmd
@@ -0,0 +1,3 @@
+REM This is work around for the Angular 11 to be working with the Net 5.0 more details https://github.com/angular/angular-cli/issues/16961
+@echo ** Angular Live Development Server is listening on localhost:%~2, open your browser on http://localhost:%~2/ **
+ng serve %1 %~2
